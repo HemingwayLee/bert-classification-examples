1b897504e19b49730603420f6cc6cb9a96c9a6b9
LeeHemingway <coherence0815@gmail.com>
2019-07-06 19:20:52 +0900

    change paramaters

--- run_en.sh
+++ run_en.sh
@@ -1,14 +1,14 @@
 export $(egrep -v '^#' .env | xargs)
 
 python3 run_classifier.py \
-  --task_name=MRPC \
+  --task_name=myproc \
   --do_train=true \
   --do_eval=true \
   --data_dir=./data_en \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-  --max_seq_length=128 \
+  --max_seq_length=4 \
   --train_batch_size=1 \
   --learning_rate=2e-5 \
   --num_train_epochs=10.0 \
