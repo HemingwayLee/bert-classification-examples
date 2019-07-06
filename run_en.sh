07581584ec8786fad73439c80fb72d524d68c899
LeeHemingway <coherence0815@gmail.com>
2019-07-06 18:30:42 +0900

    init project

---
+++ run_en.sh
@@ -0,0 +1,16 @@
+export $(egrep -v '^#' .env | xargs)
+
+python3 run_classifier.py \
+  --task_name=MRPC \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=./data_en \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=1 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=10.0 \
+  --output_dir=./bert_output/
+
