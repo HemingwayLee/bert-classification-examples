dd2b3e2f9a723611699bdf9ad669899693f008ae
LeeHemingway <coherence0815@gmail.com>
2019-07-07 22:43:11 +0900

    add chinese examples

---
+++ run_zh.sh
@@ -0,0 +1,16 @@
+export $(egrep -v '^#' .env | xargs)
+
+python3 run_classifier.py \
+  --task_name=myproc \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=./data_zh \
+  --vocab_file=$BERT_BASE_DIR_ZH/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR_ZH/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR_ZH/bert_model.ckpt \
+  --max_seq_length=10 \
+  --train_batch_size=2 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=8.0 \
+  --output_dir=./bert_output_zh/
+
